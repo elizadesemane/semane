300d71e87ed50ff4a42a98f5bcce512d6cbab107
elizadesemane <elizadesemane11@gmail.com>
2023-05-02 12:37:44 +0400

    finish

--- main.js
+++ main.js
@@ -1,74 +1,60 @@
-const add = document.querySelector('#add');
-const sort = document.querySelector('#sort');
-const list = document.querySelector('#list');
+const add = document.querySelector('.todo-btn-add');
+const sort = document.querySelector('.todo-btn-sort');
+const list = document.querySelector('.todo-list');
+let sorted = false;
 
-function addEventListItem(text,list){
+function addListItem(text,list){
     const li =document.createElement('li');
+    li.classList.add('todo-list-item');
     const span =document.createElement('span');
     span.innerText = text;
 
-    const del = document.createElement ('button');
-    del.innerText ='X';
+    const del = document.createElement('button');
+    del.classList.add('todo-list-item-btn');
     del.addEventListener('click', (e) => {
-       e.target.parentElements.remove();
+        e.target.parentElement.remove();
     });
-       li.append(del);
+
+       li.append(span, del);
        list.append(li);
 }
+
 add.addEventListener('click', () => {
-    const info = prompt();
-    addListItem(info, list);
+   const item = document.querySelector('.todo-list-item-text')
+     const info = item.value;
+     addListItem(info, list);
+        item.value = '';
+
 });
 
-sort.addEventListener('click', () => {
-    const arr =new Array () ;
-    const siyahi = document.querySelectorAll('li');
-   
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-add.addEventListener('click', () =>{
- const info = prompt ();
- const li =document.createElement('li');
- li.innerText = info;
-
- const del = document.createElement ('button');
- del.innerText ='X';
- del.addEventListener('click', (e) => {
-    e.target.parentElements.remove();
- });
-    li.append(del);
-    list.append(li);
+sort.addEventListener('click', (event) => {
+   const arr =new Array () ;
+   const siyahi = document.querySelectorAll('li');
+    siyahi.forEach(li =>{
+      arr.push(li.firstElementChild. innerText);
+    });
+    if(!sorted){
+      arr.sort((a,b)=>{
+         if (a > b) return 1;
+         if(a < b)return -1;
+            return 0;
+      });
+      sorted =true;
+      event.target.classList.remove('low');
+      event.target.classList.add('high');
+    }else{
+      arr.sort((a,b)=> {
+         if(a < b) return 1;
+         if(a > b) return -1;
+         return 0;
+      });
+      sorted =false;
+      event.target.classList.remove('high');
+      event.target.classList.add('low');
+    }
+    list.innerHTML ='';
+    arr.forEach( item => {
+      addListItem(item, list);
+    });
 });
-   sort.addEventListener
+  
